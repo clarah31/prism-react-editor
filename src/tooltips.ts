ca09881e952dc3c5349b60c55a02fa3b12130a47
mau <mau@auit.de>
2024-10-16 17:56:23 +0200

    tooltip

--- src/tooltips.ts
+++ src/tooltips.ts
@@ -1,58 +1,73 @@
-import { ReactNode, useCallback, useLayoutEffect, useMemo } from "react"
-import { PrismEditor } from "./types"
-import { createTemplate } from "./utils/local"
-import { createPortal } from "react-dom"
-import { useCursorPosition } from "./extensions/cursor"
+import { ReactNode, useCallback, useLayoutEffect, useMemo } from 'react';
+import { createPortal } from 'react-dom';
+import { useCursorPosition } from './extensions/cursor';
+import { PrismEditor } from './types';
+import { createTemplate } from './utils/local';
 
 const template = createTemplate(
-	"<div class=pce-tooltip style=z-index:5;top:auto;display:flex;overflow-x:clip><div>",
-)
+  '<div class=pce-tooltip style=z-index:5;top:auto;display:flex;overflow-x:clip><div>'
+);
 
 /**
  * Moves to tooltip to align with the cursor and shows it.
  * @param preferPlacingAboveCursor Whether the preferred position is above the cursor or not.
  */
-export type ShowTooltip = (preferPlacingAboveCursor?: boolean) => void
+export type ShowTooltip = (preferPlacingAboveCursor?: boolean) => void;
 
 /** Function removing the tooltip from the DOM. */
-export type HideTooltip = () => void
+export type HideTooltip = () => void;
 
 const show = (
-	editor: PrismEditor,
-	container: HTMLDivElement,
-	spacerStyle: CSSStyleDeclaration,
-	above?: boolean,
+  editor: PrismEditor,
+  container: HTMLDivElement,
+  spacerStyle: CSSStyleDeclaration,
+  above?: boolean
 ) => {
-	let cursor = editor.extensions.cursor
-	if (cursor) {
-		let { left, right, top, bottom, height } = cursor.getPosition()
-		// AUIT
-		//container.parentNode || editor.lines![0].append(container)
-		container.parentNode || (editor.lines && editor.lines[0] && editor.lines[0].parentNode&& editor.lines[0].parentNode.parentNode
-			&& editor.lines[0].parentNode.parentNode.append(container))
-		spacerStyle.width = '0px';//(editor.props.rtl ? right : left) + "px"
-		spacerStyle.display='none'
-		//console.log("show",top,container,container.parentElement,container.parentElement.getBoundingClientRect())
-		//container.parentElement.offsetTop
-		let placeAbove =
-			!above == top > bottom && (above ? top : bottom) < container.clientHeight ? !above : above
-			//document.body.appendChild(container);
-			container.style.position='absolute'
-		 //container.style[placeAbove ? "bottom" : "top"] = height + (placeAbove ? bottom : top) + "px"
-		//container.style[placeAbove ? "top" : "bottom"] = "auto"
-		container.style.top=''+(container.parentElement && container.parentElement.offsetTop+5.5+15.4+top)+'px';
-		container.style.left=''+((editor.props.rtl ? right : (container.parentElement&&container.parentElement.offsetLeft+left)) ) + "px";
-		container.style.maxHeight = '10em'
-		
-/* 		container.style.top='100px';
+  let cursor = editor.extensions.cursor;
+  if (cursor) {
+    let { left, right, top, bottom, height } = cursor.getPosition();
+    const { clientHeight, clientWidth } = editor.container!;
+    // AUIT
+    //container.parentNode || editor.lines![0].append(container)
+    container.parentNode ||
+      (editor.lines &&
+        editor.lines[0] &&
+        editor.lines[0].parentNode &&
+        editor.lines[0].parentNode.parentNode &&
+        editor.lines[0].parentNode.parentNode.append(container));
+    spacerStyle.width = '0px'; //(editor.props.rtl ? right : left) + "px"
+    spacerStyle.display = 'none';
+    //console.log("show",top,container,container.parentElement,container.parentElement.getBoundingClientRect())
+    //container.parentElement.offsetTop
+    let placeAbove =
+      !above == top > bottom && (above ? top : bottom) < container.clientHeight
+        ? !above
+        : above;
+    //document.body.appendChild(container);
+    container.style.position = 'absolute';
+    //container.style[placeAbove ? "bottom" : "top"] = height + (placeAbove ? bottom : top) + "px"
+    //container.style[placeAbove ? "top" : "bottom"] = "auto"
+    container.style.top =
+      '' +
+      (container.parentElement &&
+        container.parentElement.offsetTop + 5.5 + 15.4 + top) +
+      'px';
+    //		container.style.left=''+((editor.props.rtl ? right : (container.parentElement&&container.parentElement.offsetLeft+left)) ) + "px";
+    let ll = left < clientWidth - 300 ? left : clientWidth - 300;
+
+    container.style.left = '' + (editor.props.rtl ? right : ll) + 'px';
+    //container.style.maxHeight = '10em';
+
+    /* 		container.style.top='100px';
 		container.style.bottom='400px';
 		container.style.height='500px'
- */	}
-}
+ */
+  }
+};
 
 /**
  * Same as {@link useTooltip} but accepts a JSX element instead of a DOM node.
- * 
+ *
  * *Note:* Your tooltip element must have `flex-shrink: 0` if `fixedWidth` isn't set to
  * `false`.
  *
@@ -60,7 +75,7 @@ const show = (
  * @param element JSX element for the tooltip.
  * @param fixedWidth If false, the tooltip will shrink instead of getting offset to
  * the left if there's not enough space to the right of the cursor. Defaults to `true`.
- * 
+ *
  * @example
  * ```jsx
  * const [show, hide, portal] = useReactTooltip(
@@ -71,21 +86,21 @@ const show = (
  * The portal should be returned from an extension.
  */
 export const useReactTooltip = (
-	editor: PrismEditor,
-	element: ReactNode,
-	fixedWidth = true,
+  editor: PrismEditor,
+  element: ReactNode,
+  fixedWidth = true
 ): [ShowTooltip, HideTooltip, ReactNode] => {
-	const container = useMemo(template, [])
-	const spacerStyle = (container.firstChild as HTMLDivElement).style
+  const container = useMemo(template, []);
+  const spacerStyle = (container.firstChild as HTMLDivElement).style;
 
-	spacerStyle.flexShrink = fixedWidth ? "" : "0"
+  spacerStyle.flexShrink = fixedWidth ? '' : '0';
 
-	return [
-		useCallback(show.bind(editor, editor, container, spacerStyle), []),
-		useCallback(() => container.remove(), []),
-		createPortal(element, container),
-	]
-}
+  return [
+    useCallback(show.bind(editor, editor, container, spacerStyle), []),
+    useCallback(() => container.remove(), []),
+    createPortal(element, container),
+  ];
+};
 
 /**
  * Utility making it easy to add tooltips positioned on the cursor to an editor. Before you
@@ -106,29 +121,31 @@ export const useReactTooltip = (
  * @param element HTML Element for the tooltip.
  * @param fixedWidth If false, the tooltip will shrink instead of getting offset to
  * the left if there's not enough space to the right of the cursor. Defaults to `true`.
- * 
+ *
  * @example
  * const [show, hide] = useTooltip(editor, myElement)
  */
 export const useTooltip = (
-	editor: PrismEditor,
-	element: HTMLElement,
-	fixedWidth = true,
+  editor: PrismEditor,
+  element: HTMLElement,
+  fixedWidth = true
 ): [ShowTooltip, HideTooltip] => {
-	const container = useMemo(template, [])
-	const spacerStyle = (container.firstChild as HTMLDivElement).style
+  const container = useMemo(template, []);
+  const spacerStyle = (container.firstChild as HTMLDivElement).style;
 
-	useLayoutEffect(() => {
-		container.append(element)
-		return () => element.remove()
-	}, [element])
+  useLayoutEffect(() => {
+    container.append(element);
+    return () => element.remove();
+  }, [element]);
 
-	spacerStyle.flexShrink = fixedWidth ? "" : "0"
-	
-	element.style.flexShrink = fixedWidth ? "0" : ""
+  spacerStyle.flexShrink = fixedWidth ? '' : '0';
 
-	return [
-		useCallback(show.bind(editor, editor, container, spacerStyle), []),
-		useCallback( () => container.remove() , []),
-	]
-}
+  element.style.flexShrink = fixedWidth ? '0' : '';
+
+  return [
+    useCallback(show.bind(editor, editor, container, spacerStyle), []),
+    useCallback(() => {
+      container.remove(); //AUIT
+    }, []),
+  ];
+};
